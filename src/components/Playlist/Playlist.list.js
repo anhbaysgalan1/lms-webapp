c8716ab3e52e9f6e905a19617ccceea0f46c2d23
huynq <qhuydtvt@gmail.com>
2018-06-24 09:28:06 +0700

    Twitch playlist UI

--- src/components/Playlist/Playlist.list.js
+++ src/components/Playlist/Playlist.list.js
@@ -16,51 +16,62 @@ class PlayListList extends Component {
     }
   }
 
-  render() {
+  renderPlaylists() {
+
     const playlists = this.props.playlistReducer;
     if(!playlists) return <div>Loading...</div>
-    
+
+    return (
+      <div className="round-panel">
+        {
+          _.values(playlists).map((playlist, index) => {
+            return (
+              <div 
+                className="playlist-item"
+                key={playlist._id}
+                onClick={() => this.props.history.push(`${ROUTE_ADMIN_PLAYLIST_DETAIL}/${playlist._id}`)}
+              >
+                <div className="no">{ index + 1 }</div>
+                <div className="name">{ playlist.name }</div>
+                <div className="video-count">{ playlist.videos.length } videos</div>
+                <div className="controls" 
+                  onClick={(event) => {
+                  event.stopPropagation();
+                  this.props.openPopup(() => {
+                    this.props.deletePlaylist(playlist)
+                  },
+                  null);
+                }}>
+                  <div className="delete">
+                    <i className="text-dark fas fa-trash-alt"></i>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+
+  renderControls() {
+    return (
+      <div className="admin-controls">
+        <Button
+          className="admin-btn mr-2 text-dark"
+          onClick={() => this.props.history.push(ROUTE_ADMIN_PLAYLIST_NEW)}
+        >
+          <i className="fas fa-plus mr-1"></i> {'  '} Add playlist
+        </Button>
+      </div>
+    );
+  }
+
+  render() {
     return (
       <div>
-        <div className="admin-controls">
-          <Button
-            className="admin-btn mr-2 text-dark"
-            onClick={() => this.props.history.push(ROUTE_ADMIN_PLAYLIST_NEW)}
-          >
-           <i className="fas fa-plus mr-1"></i> {'  '} Add playlist
-          </Button>
-        </div>
-        <Table className="round-panel">
-          <tbody>
-          {
-            _.values(playlists).map((playlist, index) => {
-              return (
-                <tr 
-                  className="playlist-item"
-                  key={playlist._id}
-                  onClick={() => this.props.history.push(`${ROUTE_ADMIN_PLAYLIST_DETAIL}/${playlist._id}`)}
-                >
-                  <td>{ index + 1 }</td>
-                  <td>{ playlist.name }</td>
-                  <td>{ playlist.videos.length } videos</td>
-                  <td onClick={(event) => {
-                    event.stopPropagation();
-                    this.props.openPopup(() => {
-                      this.props.deletePlaylist(playlist)
-                    },
-                    null);
-                  }}>
-                    <div className="delete">
-                      <i className="text-dark fas fa-trash-alt"></i>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })
-          }
-          </tbody>
-        </Table>
-        
+        {this.renderControls()}
+        {this.renderPlaylists()}
       </div>
     );
   }
