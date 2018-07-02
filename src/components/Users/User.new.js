6ef97751473624c77f787c3aaa2a7bfde8e6d9dc
minhduc9699 <minhduc.096.99@gmail.com>
2018-07-02 18:07:39 +0700

    fix submit form

--- src/components/Users/User.new.js
+++ src/components/Users/User.new.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addUser, fetchUsers } from 'actions/user';
+import { addUser } from 'actions/user';
 import UserForm from './User.form';
-import _ from 'lodash';
-
   
   
 class UserNew extends Component {
@@ -11,27 +9,14 @@ class UserNew extends Component {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
   }
-  componentWillMount() {
-    if (!this.props.usersReducer) {
-      this.props.fetchUsers();
-    }
-  }
-
   render() {
-    const user = this.props.usersReducer;
-    const user_list = _.map(user)
-    const _id = user_list.length+1;
-    
-    
     return (
       <div>
         <UserForm 
           initialValues={{
             username: "",
             email: "",
-            password: "",
-            role: "student",
-            _id: _id
+            role: "student"
           }}
           onSubmit={this.onSubmit} 
           onCancel={this.props.history.goBack}
@@ -41,19 +26,15 @@ class UserNew extends Component {
   }
 
   onSubmit(user) {
-    console.log(user);
-    
     this.props.addUser(user);
     this.props.history.goBack();
   }
 
 }
 
-function mapConnectToReducer({usersReducer}) {
-  return {usersReducer}
-}
   
-export default connect(mapConnectToReducer, { addUser, fetchUsers })(UserNew);
+  
+export default connect(null, { addUser })(UserNew);
 
 
 
