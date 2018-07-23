6879311a66934c9b180547d597bbe07fe9d4d81c
MY-ENBF73M131K5\MyPC <donezombie@gmail.com>
2018-07-23 16:09:02 +0700

    .

--- src/statics/urls.js
+++ src/statics/urls.js
@@ -1,3 +1,6 @@
 export const ROOT_URL = "http://localhost:9000";
 export const API_URL = `${ROOT_URL}/api`;
 export const API_USER = `${API_URL}/users`;
+
+export const API_CLASSROOM = `${API_URL}/classrooms`
+export const API_CLASSROOM_COURSE = `${API_URL}/courseclassroom`
