e25430c4694ca0a25ee2aab8fbb9ec95f6edd9f3
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-07-30 18:04:46 +0700

    Update eslint airbnb && customized

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,23 @@
 module.exports = {
+    "extends": "airbnb",
     "env": {
         "browser": true,
-        "es6": true
+        "node": true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    "settings": {
+        "import/resolver": {
+          "node": {
+            "paths": ["src"]
+          }
+        }
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+        'no-underscore-dangle': ['error', {
+            allow: ["_id"],
+            allowAfterThis: false,
+            allowAfterSuper: false,
+            enforceInMethodNames: false,
+        }],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     }
 };
