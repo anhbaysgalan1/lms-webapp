b0c0060138d78c33676693025ce3a10980b35f30
MY-ENBF73M131K5\MyPC <donezombie@gmail.com>
2018-08-08 18:01:47 +0700

    .

--- src/reducers/classcourseReducer.js
+++ src/reducers/classcourseReducer.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
-import { UPDATE_COURSE_CLASSROOM, ADD_COURSE_CLASSROOM, DELETE_COURSE_CLASSROOM, FETCH_COURSE_CLASSROOM} from '../actions/classcourse';
+import {
+  UPDATE_COURSE_CLASSROOM, ADD_COURSE_CLASSROOM, DELETE_COURSE_CLASSROOM, FETCH_COURSE_CLASSROOM,
+} from '../actions/classcourse';
 
 export default function (state = null, action) {
   switch (action.type) {
-
     case FETCH_COURSE_CLASSROOM:
       return _.mapKeys(action.payload.data, '_id');
 
