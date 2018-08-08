71b4e5e491754353fc044e8cbf90f3a3b71a2be3
MY-ENBF73M131K5\MyPC <donezombie@gmail.com>
2018-08-08 17:23:31 +0700

    fixUserEslint

--- src/components/Users/User.new.js
+++ src/components/Users/User.new.js
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
-import { addUser } from 'actions/user';
+import { addUser } from '../../actions/user';
 import UserForm from './User.form';
-  
-  
+/* eslint-disable */
+import PropTypes from 'prop-types';
+/* eslint-enable */
+
 class UserNew extends Component {
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onSubmit(user) {
+    const ActionaddUser = _.get(this.props, 'addUser');
+    const { history } = this.props;
+    ActionaddUser(user).then(() => {
+      history.goBack();
+    });
+  }
+
   render() {
+    const { history } = this.props;
     return (
       <div>
-        <UserForm 
+        <UserForm
           initialValues={{
-            username: "",
-            email: "",
-            password: "",
-            role: 0
+            username: '',
+            email: '',
+            password: '',
+            role: 0,
           }}
-          onSubmit={this.onSubmit} 
-          onCancel={this.props.history.goBack}
+          onSubmit={this.onSubmit}
+          onCancel={history.goBack}
         />
       </div>
     );
   }
-
-  onSubmit(user) {
-    this.props.addUser(user).then(() => {
-    this.props.history.goBack();
-    });
-  }
-
 }
 
-  
-  
+UserNew.propTypes = {
+  history: PropTypes.shape({
+    length: PropTypes.number,
+    action: PropTypes.string,
+  }).isRequired,
+};
+
 export default connect(null, { addUser })(UserNew);
-
-
-
