6101e7df4c77dc5553f6e08525cfe29fb7e6962c
qhuydtvt <qhuydtvt@gmail.com>
2018-08-12 10:23:17 +0700

    Fix VideoItem eslint bug

--- src/components/Playlist/Playlist.form/VideoItem/index.js
+++ src/components/Playlist/Playlist.form/VideoItem/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './index.css';
 
-export default function(props) {
+const playlistItem = (props) => {
   const {
     title,
     youtubeId,
@@ -11,35 +12,61 @@ export default function(props) {
     playlistName,
     disableDelete,
     onClick,
-    selected
+    selected,
   } = props;
 
-  const wrapperClassName = selected ? "video-item highlight": "video-item";
+  const wrapperClassName = selected ? 'video-item highlight' : 'video-item';
 
   return (
-    <div className={wrapperClassName} onClick={onClick}>
+    <div
+      className={wrapperClassName}
+      onClick={onClick}
+      role="presentation"
+    >
       <div className="content">
         <div className="thumbnail">
           <img
             src={`https://i.ytimg.com/vi/${youtubeId}/mqdefault.jpg`}
             alt={`${title}`}
           />
-          <span className="duration">{duration}</span>
+          <span className="duration">
+            { duration }
+          </span>
         </div>
         <div className="brief">
-          <h5 className="title">{ title }</h5>
+          <h5 className="title">
+            { title }
+          </h5>
         </div>
       </div>
       {
-        !disableDelete &&
-        <div className="controls">
-          <i
-            className="text-secondary fas fa-times clickable"
-            onClick={onDeleteClick}
-            data-toggle="tooltip" data-placement="right" title={`Remove '${title}' from '${playlistName}'`}
-          />
-        </div>
+        !disableDelete
+        && (
+          <div className="controls">
+            <i
+              className="text-secondary fas fa-times clickable"
+              onClick={onDeleteClick}
+              role="presentation"
+              data-toggle="tooltip"
+              data-placement="right"
+              title={`Remove '${title}' from '${playlistName}'`}
+            />
+          </div>
+        )
       }
     </div>
   );
-}
+};
+
+playlistItem.propTypes = {
+  title: PropTypes.func.isRequired,
+  youtubeId: PropTypes.string.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
+  duration: PropTypes.string.isRequired,
+  playlistName: PropTypes.string.isRequired,
+  disableDelete: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
+};
+
+export default playlistItem;
