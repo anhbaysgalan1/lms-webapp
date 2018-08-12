4ba022cbd8b578679727a74ce85394a662f12214
qhuydtvt <qhuydtvt@gmail.com>
2018-08-12 11:02:36 +0700

    Fix playlist networkds

--- src/networks/playlist.js
+++ src/networks/playlist.js
@@ -1,67 +1,62 @@
 import axios from 'axios';
 import { API_PLAYLIST } from 'statics/urls';
-import { fieldsNotEmpty  } from 'utils';
+import { fieldsNotEmpty } from 'utils';
 
 axios.defaults.validateStatus = status => status < 500;
 axios.defaults.withCredentials = true;
 
 export function fetchPlaylistsPromise() {
-  return axios.get(API_PLAYLIST).then(response => {
+  return axios.get(API_PLAYLIST).then((response) => {
     if (fieldsNotEmpty(response, 'data.success', 'data.data')) {
       return response.data.data;
-    } else {
-      throw "Unsuccessfull or no data";
     }
+    throw new Error('Unsuccessfull or no data');
   });
 }
 
-export function fetchPlaylistPromise (id) {
+export function fetchPlaylistPromise(id) {
   return axios.get(`${API_PLAYLIST}/${id}`)
     .then(response => response.data)
-    .then(responseData => {
+    .then((responseData) => {
       if (!fieldsNotEmpty(responseData, 'success', 'data')) {
-        throw "Not success or data is empty";
+        throw new Error('Not success or data is empty');
       }
-
       return responseData.data;
     });
 }
 
-export function searchPlaylistPromise (terms) {
+export function searchPlaylistPromise(terms) {
   return axios.get(`${API_PLAYLIST}?q=${terms}`)
     .then(response => response.data)
-    .then(responseData => {
+    .then((responseData) => {
       if (!fieldsNotEmpty(responseData, 'success', 'data')) {
-        throw "Not success or data is empty";
+        throw new Error('Not success or data is empty');
       }
-
       return responseData.data;
     });
 }
 
-export function updatePlaylistPromise (playlist) {
-  return axios.put(`${API_PLAYLIST}/${playlist._id}`, playlist)
+export function updatePlaylistPromise(playlist) {
+  return axios.put(`${API_PLAYLIST}/${playlist._id}`, playlist);
 }
 
-export function deletePlaylistPromise (playlist) {
+export function deletePlaylistPromise(playlist) {
   return axios.delete(`${API_PLAYLIST}/${playlist._id}`)
-              .then(response => {
-                if (response.status == 204) {
-                  return playlist;
-                } else {
-                  throw "Deletion was unsuccessful";
-                }
-              });
+    .then((response) => {
+      if (response.status === 204) {
+        return playlist;
+      }
+      throw new Error('Deletion was unsuccessful');
+    });
 }
 
-export function addPlaylistPromise (playlist) {
+export function addPlaylistPromise(playlist) {
   return axios.post(`${API_PLAYLIST}`, playlist)
     .then(response => response.data)
-    .then(responseData => {
+    .then((responseData) => {
       if (!fieldsNotEmpty(responseData, 'success', 'data')) {
-        throw "Not success or data is empty";
+        throw new Error('Not success or data is empty');
       }
-
       return responseData.data;
     });
 }
