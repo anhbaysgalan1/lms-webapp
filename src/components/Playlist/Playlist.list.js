b4f637d6fcf6872680541694fabf41fb288bc876
qhuydtvt <qhuydtvt@gmail.com>
2018-08-12 10:00:25 +0700

    Fix playlist.list eslint error

--- src/components/Playlist/Playlist.list.js
+++ src/components/Playlist/Playlist.list.js
@@ -2,66 +2,92 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'reactstrap';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 
 import { fetchPlaylists, deletePlaylist } from 'actions/playlist';
 import { openPopup } from 'actions/popup';
+import SimpleLoading from '../SimpleLoading';
 import { ROUTE_ADMIN_PLAYLIST_NEW, ROUTE_ADMIN_PLAYLIST_DETAIL } from '../routes';
 
 import './Playlist.list.css';
-  
+
 class PlayListList extends Component {
   componentWillMount() {
-    if(!this.props.playlistReducer) {
-      this.props.fetchPlaylists();
+    const { playlistReducer, fetchPlaylistsAction } = this.props;
+    if (!playlistReducer) {
+      fetchPlaylistsAction();
     }
   }
 
-  renderPlaylists() {
+  renderItemControls(playlist) {
+    const {
+      openPopupAction,
+      deletePlaylistAction,
+    } = this.props;
+    return (
+      <div
+        className="controls"
+        role="presentation"
+        onClick={(event) => {
+          event.stopPropagation();
+          openPopupAction(() => deletePlaylistAction(playlist), null);
+        }}
+      >
+        <div className="delete">
+          <i className="text-dark fas fa-trash-alt" />
+        </div>
+      </div>
+    );
+  }
 
-    const playlists = this.props.playlistReducer;
-    if(!playlists) return <div>Loading...</div>
+  renderPlaylists() {
+    const {
+      playlistReducer: playlists,
+      history,
+    } = this.props;
+
+    if (!playlists) return <SimpleLoading />;
 
     return (
       <div className="round-panel">
         {
-          _.values(playlists).map((playlist, index) => {
-            return (
-              <div 
-                className="playlist-item"
-                key={playlist._id}
-                onClick={() => this.props.history.push(`${ROUTE_ADMIN_PLAYLIST_DETAIL}/${playlist._id}`)}
-              >
-                <div className="no">{ index + 1 }</div>
-                <div className="name">{ playlist.title }</div>
-                <div className="video-count">{ playlist.videos.length } videos</div>
-                <div className="controls" 
-                  onClick={(event) => {
-                  event.stopPropagation();
-                  this.props.openPopup(() => {
-                    this.props.deletePlaylist(playlist)
-                  },
-                  null);
-                }}>
-                  <div className="delete">
-                    <i className="text-dark fas fa-trash-alt"></i>
-                  </div>
-                </div>
+          _.values(playlists).map((playlist, index) => (
+            <div
+              className="playlist-item"
+              key={playlist._id}
+              role="presentation"
+              onClick={() => history.push(`${ROUTE_ADMIN_PLAYLIST_DETAIL}/${playlist._id}`)}
+            >
+              <div className="no">
+                { index + 1 }
               </div>
-            );
-          })
+              <div className="name">
+                { playlist.title }
+              </div>
+              <div className="video-count">
+                { playlist.videos.length }
+                {' '}
+                videos
+              </div>
+              { this.renderItemControls(playlist) }
+            </div>
+          ))
         }
       </div>
     );
   }
 
   renderControls() {
+    const { history } = this.props;
     return (
       <div className="admin-controls">
         <Button
           className="admin-btn mr-2 text-dark"
-          onClick={() => this.props.history.push(ROUTE_ADMIN_PLAYLIST_NEW)}
+          onClick={() => history.push(ROUTE_ADMIN_PLAYLIST_NEW)}
         >
-          <i className="fas fa-plus mr-1"></i> {'  '} Add playlist
+          <i className="fas fa-plus mr-1" />
+          {'  '}
+          Add playlist
         </Button>
       </div>
     );
@@ -82,9 +108,26 @@ function mapReducerProps({ playlistReducer }) {
 }
 
 const actions = {
-  fetchPlaylists,
-  deletePlaylist,
-  openPopup
-}
-  
+  fetchPlaylistsAction: fetchPlaylists,
+  deletePlaylistAction: deletePlaylist,
+  openPopupAction: openPopup,
+};
+
+
+PlayListList.propTypes = {
+  playlistReducer: PropTypes.shape({
+    _id: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      videos: PropTypes.array.isRequired,
+    }),
+  }).isRequired,
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+  fetchPlaylistsAction: PropTypes.func.isRequired,
+  deletePlaylistAction: PropTypes.func.isRequired,
+  openPopupAction: PropTypes.func.isRequired,
+};
+
 export default connect(mapReducerProps, actions)(PlayListList);
