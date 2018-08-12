142778532ac92c1b2b4258a44a081e7faa5f4fe0
qhuydtvt <qhuydtvt@gmail.com>
2018-08-12 09:04:33 +0700

    Fix eslint config and fix eslint for playlist index

--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import PlaylistList from './Playlist.list';
 import PlaylistNew from './Playlist.new';
@@ -6,19 +6,14 @@ import PlaylistDetail from './Playlist.detail';
 
 import { ROUTE_ADMIN_PLAYLIST_NEW, ROUTE_ADMIN_PLAYLIST_DETAIL_ID } from '../routes';
 
-class Playlist extends Component {
-  render() {
-    return (
-      <div className="h-100">
-        <Switch>
-          <Route path={ROUTE_ADMIN_PLAYLIST_NEW} component={PlaylistNew} />
-          <Route path={ROUTE_ADMIN_PLAYLIST_DETAIL_ID} component={PlaylistDetail} />
-          <Route path="/" component={PlaylistList} />
-        </Switch>
-      </div>
-    )
-  }
-}
- 
- 
-export default withRouter(Playlist);
+const playlist = () => (
+  <div className="h-100">
+    <Switch>
+      <Route path={ROUTE_ADMIN_PLAYLIST_NEW} component={PlaylistNew} />
+      <Route path={ROUTE_ADMIN_PLAYLIST_DETAIL_ID} component={PlaylistDetail} />
+      <Route path="/" component={PlaylistList} />
+    </Switch>
+  </div>
+);
+
+export default withRouter(playlist);
