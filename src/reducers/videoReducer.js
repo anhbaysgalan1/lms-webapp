073f5362319fbf8a143000f70d111452618ebf13
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-09-14 16:05:09 +0700

    a

--- src/reducers/videoReducer.js
+++ src/reducers/videoReducer.js
@@ -6,7 +6,7 @@ import {
 export default function (state = null, action) {
   switch (action.type) {
     case FETCH_VIDEOS:
-      return _.has(action.payload, 'data.data') ? _.mapKeys(action.payload.data.data, '_id') : [];
+      return _.has(action.payload, 'data.data.videos') ? _.mapKeys(action.payload.data.data.videos, '_id') : [];
     case UPDATE_VIDEO:
     case ADD_VIDEO:
       return action.payload.data.data._id ? action.payload.data.data : {};
